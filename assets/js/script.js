a92b4317f42aa9cee48457d144b54979a7534a86
SladkaMalinova <zarenacollopy@yahoo.ie>
2025-09-20 21:40:24 +0100

    JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,21 @@ let totalStats = {
   calories: 0,
   streak: 0
 };
+
+//Initialise the applicaiton
+function init {
+  loadData();
+  initChart();
+  updateStats();
+  checkAchievements();
+}
+
+//Load data from local storage
+function loadData() {
+  const savedWorkouts = JSON.parse(localStorage.getItem('fittrack_workouts') || '[]');
+  const savedStats = JSON.parse(localStorage.getItem('fittrack_stats') || '{"workouts":0,"minutes":0, "calories":0, "streak":0}');
+
+  const savedAchievements = JSON.parse(localStorage.getItem('fittrack_achievements') || '[]');
+
+  workouts = savedWorkouts
+}
